b0827c5634811acf513deba0513d5749f13a56af
C-Espriella13 <cahucaes05@gmail.com>
2024-06-11 15:12:00 -0600

    Updated Repaint
    Ahora está mejor coloreado

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import veterinarioIMG from '../images/veterinario.png';
+import veterinarioIMG from '../images/sastre.png';
 import { MDBContainer, MDBCard, MDBCardBody} from 'mdb-react-ui-kit';
 import Nav from '../components/NavigationBar';
 
@@ -10,8 +10,8 @@ function Contact(){
     <MDBContainer fluid className='d-flex align-items-center justify-content-center mt-5'>
       <MDBCard  id="contact">
         <MDBCardBody className='px-5'>
-          <img src={veterinarioIMG} alt="Veterinario1"/>
-          <p>Email: lucas_delgado@gmail</p>
+          <img src={veterinarioIMG} alt="Sastre1"/>
+          <p>Email: duncan_martinez@gmail</p>
           <p>Phone: +590 9782-1300 </p>
         </MDBCardBody>
       </MDBCard>
